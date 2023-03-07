be866065491f8504b4bd025fd847cec40dc6882d
AlfZR <Alfonso_Z.R@hotmail.com>
2023-03-07 01:15:29 -0600

    Scripts for day 1

---
+++ day1/day1.py
@@ -0,0 +1,28 @@
+#print("Hello World!\nHello World!\nHello World!")
+#print("Hello World! " + "Hello World! " + "Hello World! ")
+
+# Input
+name = input("The name: ")
+print("The name is: " + name)
+
+print("Ok, " + input("What is your age? "))
+
+# Write a program that prints the number of characters in a user's name
+
+# 1
+name = input("What is your name? ")
+print(len(name))
+
+# 2
+print(len(input("What is your name? ")))
+
+# Band name generator
+
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + pet)
+
+
